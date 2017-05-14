a5c7f1ab38bdc13d5a9a2c8d91b9a9a4747f317f
Davor Lučić <13davor@gmail.com>
2017-05-14 17:08:27 +0200

    Add files via upload

--- onlinenarudzba.sql
+++ onlinenarudzba.sql
@@ -6,40 +6,14 @@ create table korisnik(
 sifra				int not null primary key auto_increment,
 ime					varchar(50) not null,
 prezime				varchar(50) not null,
-adresa				varchar(50) not null,
-mjesto				varchar(50) not null,
-broj_telefona		varchar(50) not null
-);
-
-create table dostava(
-sifra				int not null primary key auto_increment,
 longitude			float(10,6) not null,
 latitude			float(10,6) not null,
-cijena				decimal(18,2) not null,
-predvideno_vrijeme	datetime not null,
-osobno_preuzimanje	varchar(10) not null
+broj_telefona		varchar(50) not null,
+mail				varchar(50) not null,
+password			varchar(50) not null
 );
 
-create table narudzba(
-sifra				int not null primary key auto_increment,
-korisnik			int not null,
-dostava				int not null,
-broj				char(50) not null,
-datum				datetime not null,
-ukupni_iznos		decimal(18,2) not null
-);
-
-create table jelovnik(
-sifra				int not null primary key auto_increment,
-narudzba			int not null,
-naziv				varchar(50) not null,
-cijena				decimal(18,2) not null,
-kolicina			char(20) not null,
-slika				longtext not null,
-opis				longtext null
-);
-
-create table grupa_jela(
+create table vrsta_jela(
 sifra				int not null primary key auto_increment,
 jelovnik			int not null,
 pizza				varchar(50) not null,
@@ -48,12 +22,45 @@ tjestenina			varchar(50) not null,
 sendvic				varchar(50) not null
 );
 
+create table jelo(
+sifra				int not null primary key auto_increment,
+vrsta_jela			int not null,
+naziv				varchar(50) not null,
+cijena				decimal(18,2) not null,
+kolicina			char(20) not null,
+dostava_iznad_10_km	char(20) not null,
+slika				varchar(50) not null,
+opis				longtext null,
+napomena			text null
+);
+
+create table narudzba(
+sifra				int not null primary key auto_increment,
+korisnik			int not null,
+broj				char(50) not null,
+datum				datetime not null,
+longitude			float(10,6) not null,
+latitude			float(10,6) not null,
+osobno_preuzimanje	varchar(10) not null,
+predvideno_vrijeme	datetime not null,
+iznos				decimal(18,2) not null,
+napomena			text null,
+status				varchar(50) not null
+);
+
+create table jelo_narudzba(
+jelo 				int not null,
+narudzba			int not null,
+primary key			(jelo,narudzba)
+);
+
+
+alter table jelo add foreign key (vrsta_jela) references vrsta_jela(sifra);
+
 alter table narudzba add foreign key (korisnik) references korisnik(sifra);
-alter table narudzba add foreign key (dostava) references dostava(sifra);
 
-alter table jelovnik add foreign key (narudzba) references narudzba(sifra);
-
-alter table grupa_jela add foreign key (jelovnik) references jelovnik(sifra);
+alter table jelo_narudzba add foreign key (narudzba) references narudzba(sifra);
+alter table jelo_narudzba add foreign key (jelo) references jelo(sifra);
 
 
 
